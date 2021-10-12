0afa7a4fb2d5ead4534a4f7078c92d647d329252
AakifSynavos <muhammad.akif@synavos.com>
2021-10-12 12:44:18 +0500

    initial commit

---
+++ build/PaymentGatewayFramework.xcframework/ios-arm64_x86_64-maccatalyst/PaymentGatewayFramework.framework/Versions/A/Headers/PaymentGatewayFramework.h
@@ -0,0 +1,18 @@
+//
+//  PaymentGatewayFramework.h
+//  PaymentGatewayFramework
+//
+//  Created by Aakif Nadeem on 11/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaymentGatewayFramework.
+FOUNDATION_EXPORT double PaymentGatewayFrameworkVersionNumber;
+
+//! Project version string for PaymentGatewayFramework.
+FOUNDATION_EXPORT const unsigned char PaymentGatewayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymentGatewayFramework/PublicHeader.h>
+
+
